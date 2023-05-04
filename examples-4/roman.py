5687a0774b0732fe5e80649c3c46549b7d84ccc6
Stuart Langridge <sil@kryogenix.org>
2023-05-04 22:15:12 +0100

    update input variable to be called num

--- examples-4/roman.py
+++ examples-4/roman.py
@@ -1,11 +1,11 @@
-def int_to_roman(input):
+def int_to_roman(num):
     """ Convert an integer to a Roman numeral. """
 
     ints = (1000, 900,  500, 400, 100,  90, 50,  40, 10,  9,   5,  4,   1)
     nums = ('M',  'CM', 'D', 'CD','C', 'XC','L','XL','X','IX','V','IIII','I')
     result = []
     for i in range(len(ints)):
-        count = int(input / ints[i])
+        count = int(num / ints[i])
         result.append(nums[i] * count)
-        input -= ints[i] * count
+        num -= ints[i] * count
     return ''.join(result)
